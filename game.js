ef1b063cd7a5a28dac260cd973cb44cdb74bedef
Mattheeewww30 <mateo.arcila.1985@gmail.com>
2022-10-12 22:44:57 -0500

    Primer vistazo para renderizar los obstáculos.

--- game.js
+++ game.js
@@ -15,4 +15,11 @@ function startGame(){
     canvas.setAttribute("width", canvasrSize)
     canvas.setAttribute("height", canvasrSize)
 
+    const bombSize = canvasrSize / 10
+    game.font = bombSize + "px Verdana"
+    game.textAlign = "end"
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis["X"], bombSize, bombSize * i)
+    }
+
 }
